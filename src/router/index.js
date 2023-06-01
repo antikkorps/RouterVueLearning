6bfa23ba3ae1ca6fb3c5ddd7a077ddfc9de08602
antikkorps <franckvienot7@gmail.com>
2023-06-01 17:31:13 +0200

    understanding how routing works in Vue 3

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+//DEFINE OUR ROUTING ROUTES //
+
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import AboutView from '../views/AboutView.vue';
+import CarView from '../views/CarView.vue';
+import ContactView from '../views/ContactView.vue';
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: HomeView,
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: AboutView,
+    },
+    {
+      path: '/cars/:id',
+      name: 'car',
+      component: CarView,
+      children: [
+        {
+          path: 'contact',
+          component: ContactView,
+        },
+      ],
+    },
+  ],
+});
+
+export default router;
---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import cars from '../data/cars.json';
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <div>
+    <h1>Home View</h1>
+    <div class="cars">
+      <RouterLink
+        v-for="car in cars"
+        :key="car.id"
+        href=""
+        :to="`/cars/${car.id}`"
+      >
+        {{ car.name }}</RouterLink
+      >
+      >
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.cars {
+  display: flex;
+  align-items: center;
+}
+
+.cars a {
+  text-decoration: none;
+  color: black;
+  width: 100%;
+  box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.1);
+  margin: 10px;
+}
+</style>
